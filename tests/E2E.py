0c69ae506c990a2228737bd18c39ad1b7e6d7efa
prettyleka <prettyleka@gmail.com>
2022-01-27 22:13:24 +0200

    update e2e

--- tests/E2E.py
+++ tests/E2E.py
@@ -5,7 +5,7 @@ import selenium.webdriver.remote.webelement
 class E2E:
     def test_scores_service(self):
         self.driver = selenium.webdriver.chrome.webdriver.WebDriver('/home/user/Downloads/chromedriver')
-        self.driver.get("http://172.17.0.2:5000/")
+        self.driver.get("http://0.0.0.0:5000/")
         text1 = self.driver.find_element(by="id", value='score')
         text2=text1.text
         if 1<int(text2)<1000:
