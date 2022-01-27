64a2b4cbf4feec50d867f2b19082ac912b3c4f52
prettyleka <prettyleka@gmail.com>
2022-01-28 00:51:19 +0200

    updates

--- tests/E2E.py
+++ tests/E2E.py
@@ -1,11 +1,15 @@
-import selenium.webdriver.chrome.webdriver
-import selenium.webdriver.remote.webelement
+import os
+import AutoChromedriver
+from selenium import webdriver
+SELENIUM_GRID_HOST = os.environ.get('SELENIUM_GRID_HOST', 'localhost')
 
 
 class E2E:
-    def test_scores_service(self):
-        self.driver = selenium.webdriver.chrome.webdriver.WebDriver('/home/user/Downloads/chromedriver')
-        self.driver.get("http://0.0.0.0:5000/")
+    def test_scores_service(self,url):
+        AutoChromedriver.download_chromedriver(version="97.0.4692.71", location="/usr/bin")
+        self.driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+
+        self.driver.get(url)
         text1 = self.driver.find_element(by="id", value='score')
         text2=text1.text
         if 1<int(text2)<1000:
@@ -13,12 +17,12 @@ class E2E:
         else:
             return False
 
-    def main_function(self):
-        if self.test_scores_service():
+    def main_function(self, url):
+        if self.test_scores_service(url):
             return 0
         else:
             return -1
 
 if __name__ == '__main__':
     w = E2E()
-    w.main_function()
+    w.main_function(url="http://172.17.0.4:5000/")
