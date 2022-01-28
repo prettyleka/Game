04f7202d90f143df48783baca098e22506f2df5b
prettyleka <prettyleka@gmail.com>
2022-01-28 15:22:36 +0200

    just order

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ RUN pip3 install -r requirements.txt
 COPY . .
 # Make port 5000 available to services on the same docker network
 EXPOSE 5000
-
+ENV FLASK_APP=app.py
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import *
-
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def returnHTML():
     f = open("Scores.txt", "r+")
     result=f.read()
-    html = open("app/templates/Score.html", "r+")
-    text = html.read().format(SCORE=result)
     if result != "":
-        return render_template('app/templates/Score.html')
+        return render_template('Score.html', SCORE=result)
     else:
-        return render_template('app/templates/Error.html')
+        return render_template('Error.html')
+
+
+if __name__ == "__main__":
+    app.run()
